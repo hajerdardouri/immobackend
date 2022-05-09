0d1b4ee4d4ed4b5455ec62c32eb24fcb5cdb3880
hajer dardouri <dardourihajer67@gmil.com>
2022-05-09 17:08:34 +0100

    signin,signup and creating user

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Test {
+pub struct Listing {
     pub _id: ObjectId,
     pub photo: Option<String>,
     pub title: String,
@@ -11,4 +11,12 @@ pub struct Test {
     pub bathnum: Option<String>,
     pub surfacesize: Option<String>,
     pub price: Option<String>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub _id: ObjectId,
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub account_type: String,
 }

f734457f6732500d6df7528a049e5d6583d2f506
hajer dardouri <dardourihajer67@gmil.com>
2022-05-19 12:27:43 +0100

    sign in sign up and filter

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,7 +1,11 @@
-use crate::models::{Listing, User};
-use crate::{MONGOCOLLECTIONLISTING, MONGOCOLLECTIONUSERS, MONGO_DB};
-use actix_web::{get, post, web, HttpResponse, Responder};
+use crate::models::{JWTClaim, Listing, User};
+use crate::{JWT_SECRET, MONGOCOLLECTIONLISTING, MONGOCOLLECTIONUSERS, MONGO_DB};
+use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder};
+use chrono;
+use chrono::Duration;
 use futures::StreamExt;
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use md5;
 use mongodb::bson::doc;
 use mongodb::bson::oid::ObjectId;
 use mongodb::bson::Document;
@@ -10,12 +14,13 @@ use mongodb::error::WriteFailure;
 use mongodb::Client;
 use serde::{Deserialize, Serialize};
 
-extern crate crypto;
-extern crate hyper;
-extern crate jwt;
-extern crate regex;
-
 //fetch_listing
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    company: String,
+    exp: usize,
+}
 
 #[derive(Deserialize)]
 pub struct SearchParams {
@@ -32,7 +37,9 @@ pub async fn listing(db: web::Data<Client>, qs: web::Query<SearchParams>) -> imp
         filter.insert(
             "title",
             doc! { "$regex": format!("{}", q), "$options": "i" },
-        )
+        );
+        filter.insert("location", q);
+        filter.insert("bednum", q);
     });
 
     let mut cursor = listing_collection.find(Some(filter), None).await.unwrap();
@@ -57,42 +64,68 @@ pub struct CreateListingData {
     photo: Option<String>,
     title: String,
     location: Option<String>,
-    bednum: Option<String>,
-    bathnum: Option<String>,
-    surfacesize: Option<String>,
-    price: Option<String>,
+    bednum: Option<i32>,
+    bathnum: Option<i32>,
+    surfacesize: Option<i32>,
+    price: Option<i32>,
 }
 
 #[post("api/create_listing")]
 pub async fn create_listing(
     db: web::Data<Client>,
     data: web::Json<CreateListingData>,
-) -> impl Responder {
+    request: HttpRequest,
+) -> actix_web::Result<HttpResponse> {
     let listing_collection = db
         .database(MONGO_DB)
         .collection::<Listing>(MONGOCOLLECTIONLISTING);
-    return match listing_collection
-        .insert_one(
-            Listing {
-                _id: ObjectId::new(),
-                photo: data.photo.clone(),
-                title: data.title.clone(),
-                location: data.location.clone(),
-                bednum: data.bednum.clone(),
-                bathnum: data.bathnum.clone(),
-                surfacesize: data.surfacesize.clone(),
-                price: data.price.clone(),
-            },
-            None,
-        )
-        .await
-    {
-        Ok(db_result) => {
-            println!("New document inserted with id {}", db_result.inserted_id);
-            HttpResponse::Created().body("insert_successful")
-        }
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    };
+
+    let req_headers = request.headers();
+    let basic_auth_header = req_headers.get("Authorization");
+    let basic_auth = basic_auth_header
+        .ok_or(HttpResponse::Unauthorized().body("missing_authorization_header"))?
+        .to_str()
+        .map_err(|_| HttpResponse::Unauthorized().body("invalid_authorization_header"))?
+        .replace("Bearer ", "");
+
+    let claim = decode::<JWTClaim>(
+        &basic_auth,
+        &DecodingKey::from_secret(JWT_SECRET.as_ref()),
+        &{
+            let mut validation = Validation::default();
+            validation.validate_exp = true;
+            validation
+        },
+    )
+    .map_err(|e| HttpResponse::Unauthorized().body(e.to_string()))?;
+
+    return Ok(
+        match listing_collection
+            .insert_one(
+                Listing {
+                    _id: ObjectId::new(),
+                    user_id: ObjectId::parse_str(claim.claims.sub.as_str()).map_err(|_| {
+                        HttpResponse::InternalServerError().body("could_not_parse_userId")
+                    })?,
+                    photo: data.photo.clone(),
+                    title: data.title.clone(),
+                    location: data.location.clone(),
+                    bednum: data.bednum.clone(),
+                    bathnum: data.bathnum.clone(),
+                    surfacesize: data.surfacesize.clone(),
+                    price: data.price.clone(),
+                },
+                None,
+            )
+            .await
+        {
+            Ok(db_result) => {
+                println!("New document inserted with id {}", db_result.inserted_id);
+                HttpResponse::Created().body("insert_successful")
+            }
+            Err(_) => HttpResponse::InternalServerError().finish(),
+        },
+    );
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -102,68 +135,114 @@ pub struct CreateUserData {
     pub password: String,
     pub account_type: String,
 }
-
+#[derive(Deserialize, Serialize)]
+pub struct SignupPayload {
+    token: String,
+}
 //create_user
 #[post("api/create_user")]
-pub async fn create_user(db: web::Data<Client>, user: web::Json<CreateUserData>) -> impl Responder {
+pub async fn create_user(
+    db: web::Data<Client>,
+    user: web::Json<CreateUserData>,
+) -> actix_web::Result<HttpResponse> {
     let user_collection = db
         .database(MONGO_DB)
         .collection::<User>(MONGOCOLLECTIONUSERS);
-
-    return match user_collection
-        .insert_one(
-            User {
-                _id: ObjectId::new(),
-                username: user.username.clone(),
-                email: user.email.clone(),
-                password: user.password.clone(),
-                account_type: user.account_type.clone(),
-            },
-            // doc! {"username": &users.username, "email": &users.email, "password": &users.password,
-            // "account_type": &users.account_type},
-            None,
-        )
-        .await
-    {
-        Ok(db_result) => {
-            println!("New user created with id {}", db_result.inserted_id);
-            HttpResponse::Created().body("insert_successful")
-        }
-        Err(err) => match err.kind.as_ref() {
-            Write(WriteFailure::WriteError(e)) if e.code == 11000 => {
-                HttpResponse::Unauthorized().body("username_unavailable")
-            }
-            _ => HttpResponse::InternalServerError().finish(),
-        },
+    let jwt_claim = JWTClaim {
+        aud: "public".to_string(),
+        exp: (chrono::Utc::now() + Duration::seconds(10)).timestamp() as usize,
+        iat: chrono::Utc::now().timestamp() as usize,
+        iss: "Tayara".to_string(),
+        nbf: chrono::Utc::now().timestamp() as usize,
+        sub: user.username.to_string(),
     };
+    let token = encode(
+        &Header::default(),
+        &jwt_claim,
+        &EncodingKey::from_secret(JWT_SECRET.as_ref()),
+    )
+    .map_err(|_| HttpResponse::InternalServerError().body("could_not_encode_jwt_token"))?;
+
+    Ok(
+        match user_collection
+            .insert_one(
+                User {
+                    _id: ObjectId::new(),
+                    username: user.username.clone(),
+                    email: user.email.clone(),
+                    password: format!("{:x}", md5::compute(user.password.clone())),
+                    account_type: user.account_type.clone(),
+                },
+                // doc! {"username": &users.username, "email": &users.email, "password": &users.password,
+                // "account_type": &users.account_type},
+                None,
+            )
+            .await
+        {
+            Ok(db_result) => {
+                println!("New user created with id {}", db_result.inserted_id);
+                HttpResponse::Created().body("insert_successful")
+            }
+            Err(err) => match err.kind.as_ref() {
+                Write(WriteFailure::WriteError(e)) if e.code == 11000 => {
+                    HttpResponse::Unauthorized().body("username_unavailable")
+                }
+                _ => HttpResponse::InternalServerError().finish(),
+            },
+        },
+    )
 }
 
 //Signin
 
 #[derive(Deserialize, Serialize)]
-pub struct Signin {
+pub struct SigninData {
     username: String,
     password: String,
 }
-
+#[derive(Deserialize, Serialize)]
+pub struct SigninPayload {
+    token: String,
+}
 #[post("api/signin")]
-pub async fn signin(db: web::Data<Client>, user: web::Query<Signin>) -> impl Responder {
+pub async fn signin(
+    db: web::Data<Client>,
+    user: web::Json<SigninData>,
+) -> actix_web::Result<HttpResponse> {
     let users_collection = db
         .database(MONGO_DB)
         .collection::<User>(MONGOCOLLECTIONUSERS);
     let mut filter = Document::new();
     filter.insert("username", user.username.clone());
     let db_user = users_collection.find_one(Some(filter), None).await.unwrap();
+
+    let md5password = format!("{:x}", md5::compute(user.password.clone()));
+
     if let Some(u) = db_user {
-        if u.password == user.password {
+        if u.password == md5password {
             // success
-            HttpResponse::Ok().json(u)
+            let jwt_claim = JWTClaim {
+                aud: "public".to_string(),
+                exp: (chrono::Utc::now() + Duration::seconds(10)).timestamp() as usize,
+                iat: chrono::Utc::now().timestamp() as usize,
+                iss: "Tayara".to_string(),
+                nbf: chrono::Utc::now().timestamp() as usize,
+                sub: u._id.to_string(),
+            };
+            let token = encode(
+                &Header::default(),
+                &jwt_claim,
+                &EncodingKey::from_secret(JWT_SECRET.as_ref()),
+            )
+            .map_err(|_| HttpResponse::InternalServerError().body("could_not_encode_jwt_token"))?;
+
+            Ok(HttpResponse::Ok().json(SigninPayload { token }))
         } else {
             // passsword error
-            HttpResponse::Unauthorized().body("invalid_password")
+            Ok(HttpResponse::Unauthorized().body("invalid_password"))
         }
     } else {
         // username error
-        HttpResponse::Unauthorized().body("invalid_username")
+        Ok(HttpResponse::Unauthorized().body("invalid_username"))
     }
 }
